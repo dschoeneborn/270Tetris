e22a6bda37b534b846e5d5ae98b44de95d98bec2
Dennis Schöneborn <mail@dennis-schoeneborn.de>
2017-05-31 11:44:34 +0200

    Created Demo Tetris Assets

---
+++ Assets/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public GameObject[] groups;
+	// Use this for initialization
+	void Start () {
+        spawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void spawnNext()
+    {
+        int i = Random.Range(0, groups.Length);
+
+        Instantiate(groups[i], transform.position, Quaternion.identity);
+    }
+}
