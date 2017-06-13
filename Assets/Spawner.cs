2f45b16bcc34e99fefb56c36eba6ae9de4c56109
pi <merschibjoern@aol.de>
2017-06-13 17:08:22 +0200

    Game-Over-Image hinzugefügt, Szene spielbar gemacht

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour {
     public Group[] groups;
-    public Vector3 startPosition;
     public Grid controller;
 
 	// Use this for initialization
@@ -21,7 +20,7 @@ public class Spawner : MonoBehaviour {
     {
         int i = Random.Range(0, groups.Length);
 
-        Group created = Instantiate(groups[i], startPosition, Quaternion.identity);
+        Group created = Instantiate(groups[i], transform.position, Quaternion.identity);
 
         created.GameController = controller;
     }
